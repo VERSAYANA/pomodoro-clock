67a0d30512a9633b4aec8cd05a00489c95027c7c
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-15 01:25:48 +0430

    Modify Length & App.js to use LengthContainer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import TimerWrapper from './components/TimerWrapper/TimerWrapper';
-import Length from './components/Length/Length';
+import LengthContainer from './containers/LengthContainer/LengthContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Length title="Session" value={25} />
+        <LengthContainer title="Session" />
         <TimerWrapper />
-        <Length title="Break" value={5} />
+        <LengthContainer title="Break" />
       </div>
     );
   }
--- src/components/Length/Length.js
+++ src/components/Length/Length.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Length.css';
 
-const Length = ({ title, value }) => {
+const Length = ({ title, value, increment, decrement }) => {
   return (
     <section className="length">
       <header>{title} Length</header>
 
       <div className="length-actions">
-        <span id="session-decrement">
+        <span id="session-decrement" onClick={decrement}>
           <svg viewBox="0 0 24 24">
             <path d="M19,13H5V11H19V13Z" />
           </svg>
         </span>
+
         <span id="session-length">{value}</span>
-        <span id="session-increment">
+
+        <span id="session-increment" onClick={increment}>
           <svg viewBox="0 0 24 24">
             <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
           </svg>
