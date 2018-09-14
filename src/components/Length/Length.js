271f3dd41346973e65a1fa295293df02f1f8a668
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-14 21:29:27 +0430

    Add Length component

---
+++ src/components/Length/Length.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Length.css';
+
+const Length = () => {
+  return (
+    <section className="length">
+      <header>Session Length</header>
+
+      <div className="length-actions">
+        <span id="session-decrement">
+          <svg viewBox="0 0 24 24">
+            <path d="M19,13H5V11H19V13Z" />
+          </svg>
+        </span>
+        <span id="session-length">25</span>
+        <span id="session-increment">
+          <svg viewBox="0 0 24 24">
+            <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+          </svg>
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default Length;
