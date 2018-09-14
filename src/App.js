4c521b4733a1f9eea89333814f6f7d1eafa0b1d2
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-14 19:56:37 +0430

    Add basic Timer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Timer from './components/Timer/Timer';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Timer />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Timer.css';
+
+const Timer = () => {
+  return (
+    <section id="timer">
+      <span id="timer-label">Session</span>
+      <span id="time-left">25:00</span>
+    </section>
+  );
+};
+
+export default Timer;
