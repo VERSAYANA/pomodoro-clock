e1f4f829a35036a111f3fdf2a70f16c293d5fe6e
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-14 21:36:18 +0430

    Structure App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import TimerWrapper from './components/TimerWrapper/TimerWrapper';
+import Length from './components/Length/Length';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Length title="Session" value={25} />
         <TimerWrapper />
+        <Length title="Break" value={5} />
       </div>
     );
   }
