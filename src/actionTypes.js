93dcc90aa25d92877f15215fafec73e551b37653
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-15 13:51:16 +0430

    Add action types for start_stop & reset_timer

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -3,3 +3,6 @@ export const DECREMENT_SESSION = 'DECREMENT_SESSION';
 
 export const INCREMENT_BREAK = 'INCREMENT_BREAK';
 export const DECREMENT_BREAK = 'DECREMENT_BREAK';
+
+export const START_STOP = 'START_STOP';
+export const RESET_TIMER = 'RESET_TIMER';
