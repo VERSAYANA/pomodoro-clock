a3228e30b1a240774582e168dadfd58ca76d6e22
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-15 14:23:59 +0430

    Add reducer for RESET_TIMER

--- src/reducer.js
+++ src/reducer.js
@@ -4,12 +4,15 @@ import {
   INCREMENT_BREAK,
   DECREMENT_BREAK,
   START_STOP,
+  RESET_TIMER,
 } from './actionTypes';
 
 const initialState = {
   sessionLength: 25,
   breakLength: 5,
   paused: true,
+  timerLabel: 'Session',
+  timeLeft: 1500,
 };
 
 const reducer = (state = initialState, action) => {
@@ -41,6 +44,20 @@ const reducer = (state = initialState, action) => {
         ...state,
         paused: !state.paused,
       };
+
+    case RESET_TIMER:
+      if (state.timerLabel === 'Session') {
+        return {
+          ...state,
+          timeLeft: state.sessionLength * 60,
+        };
+      } else if (state.timerLabel === 'Break') {
+        return {
+          ...state,
+          timeLeft: state.breakLength * 60,
+        };
+      }
+
     default:
       return state;
   }
